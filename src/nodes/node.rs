2a11ea51b3b81d81c367f0402e142909e8086498
Arnaud Sene <arnaud.sene@pm.me>
2024-11-19 11:42:37 -0500

    feat: Add identify node feature

--- src/nodes/node.rs
+++ src/nodes/node.rs
@@ -1,6 +1,6 @@
 use futures::StreamExt;
 use libp2p::{
-    noise, ping, rendezvous, swarm::{NetworkBehaviour, SwarmEvent}, tcp, yamux, Multiaddr, PeerId
+    identify, noise, ping, rendezvous, swarm::{NetworkBehaviour, SwarmEvent}, tcp, yamux, Multiaddr, PeerId
 };
 use std::time::Duration;
 
@@ -9,6 +9,7 @@ use crate::utils::peer_id::{read_keypair_from_file, DefaultUserDirectoryProvider
 
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
+    identify: identify::Behaviour,
     rendezvous: rendezvous::client::Behaviour,
     ping: ping::Behaviour,
 }
@@ -35,6 +36,10 @@ pub async fn run(
             yamux::Config::default,
         )?
         .with_behaviour(|key| MyBehaviour {
+            identify: identify::Behaviour::new(identify::Config::new(
+                "chaincraft-node/1.0.0".to_string(),
+                key.public(),
+            )),
             rendezvous: rendezvous::client::Behaviour::new(key.clone()),
             ping: ping::Behaviour::new(ping::Config::new().with_interval(Duration::from_secs(1))),
         })?
@@ -44,12 +49,13 @@ pub async fn run(
     // In production the external address should be the publicly facing IP address of the rendezvous point.
     // This address is recorded in the registration entry by the rendezvous point.
     let external_address = format!("/ip4/{external_address}/tcp/0").parse::<Multiaddr>().unwrap();
+    let _ = swarm.listen_on(format!("/ip4/{external_address}/tcp/0").parse().unwrap());
     swarm.add_external_address(external_address);
     swarm.dial(rendezvous_point_address.clone()).unwrap();
 
     while let Some(event) = swarm.next().await {
         match event {
-            // 1
+            // 1 Connection to rendezvous point
             SwarmEvent::ConnectionEstablished { peer_id, .. } if peer_id == rendezvous_point_peer_id => {
                 if let Err(error) = swarm.behaviour_mut().rendezvous.register(
                     rendezvous::Namespace::from_static("rendezvous"),
@@ -61,8 +67,7 @@ pub async fn run(
                 }
                 tracing::info!("Connection established with rendezvous point {}", peer_id);
             }
-            // 2
-            // once `/identify` did its job, we know our external address and can register
+            // 2 Registration to rendezvous point
             SwarmEvent::Behaviour(MyBehaviourEvent::Rendezvous(
                 rendezvous::client::Event::Registered {
                     namespace,
@@ -77,7 +82,7 @@ pub async fn run(
                     ttl
                 );
             }
-            // 3
+            // 3 Closing connection to rendezvous point
             SwarmEvent::ConnectionClosed {
                 peer_id,
                 cause: Some(error),
@@ -86,6 +91,23 @@ pub async fn run(
                 tracing::error!("Lost connection to rendezvous point {}", error);
             }
             // 4
+            // once `/identify` did its job, we know our external address and can register
+            SwarmEvent::Behaviour(MyBehaviourEvent::Identify(identify::Event::Received {
+                info,
+                ..
+            })) => {
+                // Register our external address. Needs to be done explicitly
+                // for this case, as it's a local address.
+                swarm.add_external_address(info.observed_addr);
+                if let Err(error) = swarm.behaviour_mut().rendezvous.register(
+                    rendezvous::Namespace::from_static("rendezvous"),
+                    rendezvous_point_peer_id,
+                    None,
+                ) {
+                    tracing::error!("Failed to register: {error}");
+                    return Err(Box::new(error) as Box<dyn std::error::Error>);
+                }
+            }
             SwarmEvent::NewListenAddr { address, .. } => {
                 tracing::info!("Listening on {}", address);
             }
